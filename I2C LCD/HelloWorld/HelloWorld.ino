e6a761362b5b7a79bb37c915eb54358082e5f75f
quangcaptain26-3 <124708258+quangcaptain26-3@users.noreply.github.com>
2025-04-06 22:29:49 +0700

    Add files via upload

---
+++ I2C LCD/HelloWorld/HelloWorld.ino
@@ -0,0 +1,22 @@
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 if 0x3F not working
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(1,1);
+  lcd.print("RG Innovations");
+
+}
+
+
+void loop()
+{
+}
